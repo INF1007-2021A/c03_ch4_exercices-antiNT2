d1d72abe234c906f2500ed1b601b8b92f88ec956
antiNT <32984684+antiNT2@users.noreply.github.com>
2021-09-13 13:52:08 -0400

    finished exercices

--- exercice.py
+++ exercice.py
@@ -3,23 +3,28 @@
 
 
 def is_even_len(string: str) -> bool:
-    return False
+    return (len(string) % 2 == 0)
 
 
 def remove_third_char(string: str) -> str:
-    return ""
+    output = string[:2] + string[2+1:]
+    return output
 
 
-def replace_char(string: str, old_char: str, new_char: str) -> str:
-    return ""
+def replace_char(string: str, old_char: str, new_char: str) -> str:   
+    return string.replace(old_char, new_char)
 
 
 def get_nb_char(string: str, char: str) -> int:
-    return 0
+    count = 0
+    for _char in string:
+        if(_char == char):
+            count += 1
+    return count
 
 
 def get_nb_words(sentence: str) -> int:
-    return 0
+    return len(sentence.split())
 
 
 def main() -> None:
@@ -28,15 +33,19 @@ def main() -> None:
     print(f"Le nombre de caractère dans la chaine '{string}' est {parity}")
 
     string = "Sam est cool!"
-    print(f"On supprime le 3e caratère dans la chaine '{string}'. Résultat: {remove_third_char(string)}")
+    print(
+        f"On supprime le 3e caratère dans la chaine '{string}'. Résultat: {remove_third_char(string)}")
 
     string = "hello world!"
-    print(f"On remplace le caratère w par le caractère z dans la chaine: '{string}'. Résultat: {replace_char(string, 'w', 'z')}")
+    print(
+        f"On remplace le caratère w par le caractère z dans la chaine: '{string}'. Résultat: {replace_char(string, 'w', 'z')}")
 
-    print(f"Le nombre d'occurrence de l dans hello est : {get_nb_char(string, 'l')}")
-    
+    print(
+        f"Le nombre d'occurrence de l dans {string} est : {get_nb_char(string, 'l')}")
+
     string = "Baby shark doo doo doo doo doo doo"
-    print(f"Le nombre de mots dans la chaine {string} est: {get_nb_words(string)}")
+    print(
+        f"Le nombre de mots dans la chaine {string} est: {get_nb_words(string)}")
 
 
 if __name__ == '__main__':
